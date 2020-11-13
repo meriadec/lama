0e1e68c69cfef322f8ad1d2f413a6ecd0cfbcc93
Kévin Rauscher <73123110+krauscher-ledger@users.noreply.github.com>
2020-11-13 19:45:53 +0100

    Update scalatest to 3.2.3 (#96)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -59,7 +59,7 @@ object Dependencies extends DependencyBuilders with LibraryManagementSyntax {
     "net.debasishg" %% "redisclient" % scalaRedisVersion
   )
 
-  val scalaTestVersion     = "3.2.2"
+  val scalaTestVersion     = "3.2.3"
   val scalaTestPlusVersion = "3.2.2.0"
   val scalaCheckVersion    = "1.15.0"
   val otjPgEmbeddedVersion = "0.13.3"
