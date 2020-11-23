36322f848ca1c4e154ef98f894a96a1567777c03
Kévin Rauscher <73123110+krauscher-ledger@users.noreply.github.com>
2020-11-23 16:35:31 +0100

    Update flyway-core to 7.2.1 (#103)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,7 +20,7 @@ object Dependencies extends DependencyBuilders with LibraryManagementSyntax {
   )
 
   val H2Version     = "1.4.200"
-  val flywayVersion = "7.2.0"
+  val flywayVersion = "7.2.1"
   val doobieVersion = "0.9.2"
   val postgres: Seq[ModuleID] = Seq(
     "com.h2database" % "h2"              % H2Version,
